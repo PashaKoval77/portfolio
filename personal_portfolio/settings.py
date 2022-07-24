1d332746b45152584238bd4189deeae0f9732369
PashaKoval77 <Pasha21968@4b5d82362e5e>
2022-07-24 14:13:48 +0000

    First commit

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#f#*ngdajf#brq679th1#^liyd3-xv+b0p1=w$ke(olys^h#3e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pasha21968.pythonanywhere.com']
 
 # Application definition
 
@@ -112,7 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
